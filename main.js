b77843b2aa0e0a179f77fcf5cb8fb8ab0b271144
Nick Leach <nickleach22@gmail.com>
2015-05-12 16:11:54 -0400

    Finished Normal Mode

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
-function add (numOne,numTwo) {
-
+var numOne = document.getElementById("num_one");
+var numTwo = document.getElementById("num_two");
+var calculateBtn= document.getElementById("calculateBtn");
+var ans= document.getElementById("answer");
+function add(){
+  var inputOne= Number(numOne.value);
+  var inputTwo= Number(numTwo.value);
+  var sum= inputOne + inputTwo;
+  answer.innerHTML = sum.toString();
 }
+calculateBtn.addEventListener("click", add);
